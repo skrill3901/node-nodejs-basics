77ed21e28749c6a8972138391527b0baa6726f41
Kirill Smetanin <skrill3901@gmail.com>
2022-12-05 21:18:38 +0300

    feat: args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+import { argv } from 'process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const dataArray = [];
+
+  argv.forEach((arg, i) => {
+    if (arg.startsWith('--')) {
+      dataArray.push(`${arg.slice(2)} is ${argv[i + 1] || 'not value'}`);
+    }
+  });
+
+  console.log(dataArray.join(','));
 };
 
 parseArgs();
