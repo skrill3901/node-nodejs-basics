a58cb6d4a6046d2ec35a060be14fd510c33fa762
Kirill Smetanin <skrill3901@gmail.com>
2022-12-05 20:40:54 +0300

    feat: read

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,17 +1,17 @@
-import { writeFile } from "fs/promises";
-import path from "path";
+import { writeFile } from 'fs/promises';
+import path from 'path';
 
-import { checkFileOrPath, getPathAndFilename } from "../misc/index.js";
+import { checkFileOrPath, getPathAndFilename } from '../misc/index.js';
 
 const create = async () => {
   const { __dirname } = getPathAndFilename(import.meta.url);
-  const neededPath = path.resolve(__dirname, "files");
+  const neededPath = path.resolve(__dirname, 'files');
 
-  const FILE_NAME = path.resolve(neededPath, "fresh.txt");
-  const FILE_DATA = "I am fresh and young";
+  const FILE_NAME = path.resolve(neededPath, 'fresh.txt');
+  const FILE_DATA = 'I am fresh and young';
 
   const checkFreshFile = await checkFileOrPath(FILE_NAME);
-  if (checkFreshFile) throw new Error("FS operation failed");
+  if (checkFreshFile) throw new Error('FS operation failed');
 
   await writeFile(FILE_NAME, FILE_DATA);
 };
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,19 +1,19 @@
-import { rename as fsRename } from "fs/promises";
-import path from "path";
+import { rename as fsRename } from 'fs/promises';
+import path from 'path';
 
-import { checkFileOrPath, getPathAndFilename } from "../misc/index.js";
+import { checkFileOrPath, getPathAndFilename } from '../misc/index.js';
 
 const rename = async () => {
   const { __dirname } = getPathAndFilename(import.meta.url);
 
-  const WRONG_FILE = path.resolve(__dirname, "files", "wrongFilename.txt");
-  const PROPER_FILE = path.resolve(__dirname, "files", "properFilename.md");
+  const WRONG_FILE = path.resolve(__dirname, 'files', 'wrongFilename.txt');
+  const PROPER_FILE = path.resolve(__dirname, 'files', 'properFilename.md');
 
   const isWrongFile = await checkFileOrPath(WRONG_FILE);
   const isProperFile = await checkFileOrPath(PROPER_FILE);
 
   if (isWrongFile && !isProperFile) await fsRename(WRONG_FILE, PROPER_FILE);
-  else throw new Error("FS operation failed");
+  else throw new Error('FS operation failed');
 };
 
 await rename();
