5b6290f72df472e7bb7abc16e20a8656dc86d710
Kirill Smetanin <skrill3901@gmail.com>
2023-08-13 23:07:23 +0300

    feat: zlib compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import path from "path";
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+
+import { getPathAndFilename } from "../misc/index.js";
+
+const { __dirname } = getPathAndFilename(import.meta.url);
+const FILE_TO_COMPRESS = path.resolve(__dirname, "files", "fileToCompress.txt");
+const COMPRESSED_FILE = path.resolve(__dirname, "files", "archive.gz");
+
+const gzip = createGzip();
+const source = createReadStream(FILE_TO_COMPRESS);
+const destination = createWriteStream(COMPRESSED_FILE);
+
 const compress = async () => {
-    // Write your code here 
+  await pipeline(source, gzip, destination);
 };
 
 await compress();
