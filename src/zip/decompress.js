9e9f74e10dfd5212569c7564b424e9c4e0368635
Kirill Smetanin <skrill3901@gmail.com>
2023-08-13 23:16:45 +0300

    feat: zlib decomress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
-const decompress = async () => {
-    // Write your code here 
+import path from "path";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+
+import { getPathAndFilename } from "../misc/index.js";
+
+const { __dirname } = getPathAndFilename(import.meta.url);
+const FILE_TO_DECOMRESS = path.resolve(__dirname, "files", "archive.gz");
+const PATH_TO_FILES = path.resolve(__dirname, "files", "fileToCompress.txt");
+
+const gunzip = createGunzip();
+const source = createReadStream(FILE_TO_DECOMRESS);
+const destination = createWriteStream(PATH_TO_FILES);
+
+const decomress = async () => {
+  await pipeline(source, gunzip, destination);
 };
 
-await decompress();
+await decomress();
