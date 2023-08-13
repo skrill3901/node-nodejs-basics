19e309126e92903d993ae398f07e87061eee96a2
Kirill Smetanin <skrill3901@gmail.com>
2023-08-13 22:48:15 +0300

    feat: transform stram

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
+const reverseString = (str) => str.split("").reverse().join("");
+
 const transform = async () => {
-    // Write your code here 
+  const reverseData = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, reverseString(chunk.toString()));
+    },
+  });
+
+  await pipeline(process.stdin, reverseData, process.stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,14 +1,13 @@
-import { createWriteStream } from 'fs';
-import path from 'path';
-import { stdin } from 'process';
+import { createWriteStream } from "fs";
+import path from "path";
 
-import { getPathAndFilename } from '../misc/index.js';
+import { getPathAndFilename } from "../misc/index.js";
+
+const { __dirname } = getPathAndFilename(import.meta.url);
+const FILE = path.resolve(__dirname, "files", "fileToWrite.txt");
 
 const write = async () => {
-  const { __dirname } = getPathAndFilename(import.meta.url);
-  const FILE = path.resolve(__dirname, 'files', 'fileToWrite.txt');
-
-  stdin.pipe(createWriteStream(FILE));
+  process.stdin.pipe(createWriteStream(FILE));
 };
 
 await write();
