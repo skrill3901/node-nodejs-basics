7c0a4027ff5523a389a8c7a0d14547f2d3b03149
Kirill Smetanin <skrill3901@gmail.com>
2023-08-14 22:16:59 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import path from "path";
+import { fork } from "child_process";
+
+import { getPathAndFilename } from "../misc/index.js";
+
+const { __dirname } = getPathAndFilename(import.meta.url);
+const SCRIPT_PATH = path.resolve(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = fork(SCRIPT_PATH, args, { silent: true });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["one", "two"]);
